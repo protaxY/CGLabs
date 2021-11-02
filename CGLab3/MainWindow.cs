e10dac2e009fa58ec50e9ddcbd18df16d9518f08
prota <feorov2001@mail.ru>
2021-11-02 21:38:20 +0300

    color bug fix

--- CGLab3/MainWindow.cs
+++ CGLab3/MainWindow.cs
@@ -234,30 +234,45 @@ namespace CG
                     (int)_meridiansCount.Value, 
                     (int)_parallelsCount.Value);
                     _figure.TriangulateSquares();
+                    _figure.SetColor((float) _materialColorR.Value,
+                                     (float) _materialColorG.Value,
+                                     (float) _materialColorB.Value);
                     _canvas.QueueDraw();};
             _b.ValueChanged += (o, args) => { _figure = new Ellipsoid((float)_a.Value, 
                     (float)_b.Value, (float)_c.Value, 
                     (int)_meridiansCount.Value, 
                     (int)_parallelsCount.Value);
                     _figure.TriangulateSquares();
+                    _figure.SetColor((float) _materialColorR.Value,
+                        (float) _materialColorG.Value,
+                        (float) _materialColorB.Value);
                     _canvas.QueueDraw();};
             _c.ValueChanged += (o, args) => { _figure = new Ellipsoid((float)_a.Value, 
                     (float)_b.Value, (float)_c.Value, 
                     (int)_meridiansCount.Value, 
                     (int)_parallelsCount.Value);
                     _figure.TriangulateSquares();
+                    _figure.SetColor((float) _materialColorR.Value,
+                                     (float) _materialColorG.Value,
+                                     (float) _materialColorB.Value);
                     _canvas.QueueDraw();};
             _meridiansCount.ValueChanged += (o, args) => { _figure = new Ellipsoid((float)_a.Value, 
                     (float)_b.Value, (float)_c.Value, 
                     (int)_meridiansCount.Value, 
                     (int)_parallelsCount.Value);
                     _figure.TriangulateSquares();
+                    _figure.SetColor((float) _materialColorR.Value,
+                                     (float) _materialColorG.Value,
+                                     (float) _materialColorB.Value);
                     _canvas.QueueDraw();};
             _parallelsCount.ValueChanged += (o, args) => { _figure = new Ellipsoid((float)_a.Value, 
                     (float)_b.Value, (float)_c.Value, 
                     (int)_meridiansCount.Value, 
                     (int)_parallelsCount.Value);
                     _figure.TriangulateSquares();
+                    _figure.SetColor((float) _materialColorR.Value,
+                                     (float) _materialColorG.Value,
+                                     (float) _materialColorB.Value);
                     _canvas.QueueDraw();};
 
             _materialColorR.ValueChanged += (o, args) => {_figure.SetColor((float)_materialColorR.Value, 
